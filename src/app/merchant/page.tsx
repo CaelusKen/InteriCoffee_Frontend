31771208015cc9d71c63dc97bb48505cb3c486f5
CaelusKen <caelus.khangthuan1209@gmail.com>
2024-09-30 22:19:06 +0700

    chore: create merchant options UI

---
+++ src/app/merchant/page.tsx
@@ -0,0 +1,10 @@
+import Merchant from '@/components/custom/sections/body/merchant/merchant'
+import React from 'react'
+
+export default function MerchantHomePage() {
+  return (
+    <main>
+      <Merchant />
+    </main>
+  )
+}
