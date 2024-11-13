2023d1d19bed4ae5cf7729342805da2eba9ed8d1
CaelusKen <caelus.khangthuan1209@gmail.com>
2024-11-14 01:15:43 +0700

    chore: setting up for creating cart items, proceed to checkout

---
+++ src/app/(customer)/customer/checkout/page.tsx
@@ -0,0 +1,9 @@
+import { CheckoutForm } from '@/components/custom/forms/checkout-form'
+
+export default function CheckoutPage() {
+  return (
+    <main className="container mx-auto px-4 py-8">
+      <CheckoutForm />
+    </main>
+  )
+}
